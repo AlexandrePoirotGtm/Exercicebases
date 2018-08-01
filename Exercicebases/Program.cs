c4d1659f67ea8d31b6f8673570e289b1c0c0c077
Hatem sat <Adminl@PARME22>
2018-08-01 14:19:01 +0200

    WhileForDoWhile

--- Exercicebases/Program.cs
+++ Exercicebases/Program.cs
@@ -10,6 +10,59 @@ namespace Exercicebases
     {
         static void Main(string[] args)
         {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            AfficheFor(a, b);
+            AfficheWhile(a, b);
+            AfficheDoWhile(a, b);
+
+        }
+        static void AfficheFor(int a,int b)
+        {
+            if(a<b)
+            for (int i = a;i <= b;i++)
+            {
+                Console.WriteLine("For : "+i);
+            }
+            else
+                for (int i = a; i >= b; i--)
+                {
+                    Console.WriteLine("For : " + i);
+                }
+        }
+        static void AfficheWhile(int a, int b)
+        {
+            int ab = a;
+            if (a < b)
+                while (ab <= b)
+                {
+                    Console.WriteLine("While : " + ab);
+                    ab++;
+                }
+            else
+                while (ab >= b)
+                {
+                    Console.WriteLine("While : " + ab);
+                    ab--;
+                }
+        }
+
+        static void AfficheDoWhile(int a,int b)
+        {
+            int ab = a;
+            if (a < b)
+                do
+            {
+                Console.WriteLine("Do While : " + ab);
+                ab++;
+            } while (ab <= b);
+            else
+                do
+                {
+                    Console.WriteLine("Do While : " + ab);
+                    ab--;
+                } while (ab >= b);
         }
     }
 }
